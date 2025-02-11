f192b4929e015c53d2f579dcd574414ad344a5a9
vinh-2k6 <24021677@vnu.edu.vn>
2025-02-11 23:34:38 +0700

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,34 @@
+/*
+-----------------------------------------
+🛸    🌎    ★    🛰    °    🚀    ✯
+    ★     °   ☄    ✯    ★    °    🪐
+✯    🚀    •　  🌓   °    🛰　  •   ☄
+_______________________________________
+*/
+#include<bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define N 3005
+#define all(X) X.begin(),X.end()
+using namespace std;
+const ll MOD = 1e9 + 7;
+const int maxN = 1e6 + 68;
+const ll INF = 1e15;
+const double pi = 3.14159265358979323846;
+int n;
+ll a[N], b[N], f[N], w[N];
+signed main() {
+    //freopen("dpmi.inp","r",stdin);
+    //freopen("dpmi.out","w",stdout);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    getline(cin, s);
+    cout << "Hello, World." << '\n';
+    cout << s;
+    return 0;
+}
